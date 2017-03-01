39d17003accc353ff5061b06a7966b6f9fb6cd7e
EmmanuelSGz <emmanuel.sgz360@gmail.com>
2017-03-01 16:13:51 -0400

    Add files via upload

---
+++ Cmain.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Cmain.aspx.cs" Inherits="Cmain" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css"/>
+    <title>Practica I</title>
+    <style type="text/css">
+        .auto-style1 {
+            left: 170px;
+            top: 70px;
+            height: 236px;
+            width: 423px;
+        }
+        .auto-style2 {
+            height: 167px;
+            width: 173px;
+        }
+    </style>
+</head>
+<body style="height: 227px; width: 771px;">
+    <form id="form1" runat="server">
+        <h1>Convertidor De Unidades</h1>
+        <div class="auto-style2">
+        <asp:Label ID="Label1" runat="server" Text="Inserte Valores:"></asp:Label>
+        <br />
+        <asp:TextBox class="corvetextbox" ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Convertir" Width="125px" OnClick="Button1_Click" />
+            <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Equivale A:"></asp:Label>
+        <br />
+        <asp:TextBox class="corvetextbox" ID="TextBox2" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+    </div>
+        <div id="divtop" class="auto-style1" >
+            <asp:RadioButton ID="RadioButton1" runat="server" Text="Onzas A Libras"/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<asp:RadioButton ID="RadioButton6" runat="server" Text="Metros A Pies"/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;<asp:RadioButton ID="RadioButton10" runat="server" Text="Pesos A Dolares"/>
+            <br />
+            <asp:RadioButton ID="RadioButton2" runat="server" Text="Libras A Kilogramos"/>
+            &nbsp;&nbsp;<asp:RadioButton ID="RadioButton7" runat="server" Text="Pulgadas A Pies"/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RadioButton ID="RadioButton11" runat="server" Text="Euros A Pesos"/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <asp:RadioButton ID="RadioButton3" runat="server" Text="Libras A Onzas"/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+            <asp:RadioButton ID="RadioButton8" runat="server" Text="Metros A Kilometros"/>
+            &nbsp;
+            <asp:RadioButton ID="RadioButton12" runat="server" Text="Dolares A Pesos" />
+            <br />
+            <asp:RadioButton ID="RadioButton4" runat="server" Text="Libra A Gramos"/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RadioButton ID="RadioButton9" runat="server" Text="Kilometros A Pulgadas" Checked="false"/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <asp:RadioButton ID="RadioButton5" runat="server" Text="Kilogramos A Onza"/>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Cmain.aspx.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Cmain : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        double onza, libra, kilogramo;
+        double kilometros, pulgadas, pies;
+        double pesos, euros;
+        double valor, resultado;
+
+        onza = 28.38; libra = 16.00;kilogramo=1.62;
+        pies = 0.305; kilometros = 100.00; pulgadas = 2.54; 
+        pesos = 0.0226; euros = 59.15;
+        valor = double.Parse(TextBox1.Text);
+        
+        if(RadioButton1.Checked == true){
+            resultado = valor / libra;
+            TextBox2.Text = resultado.ToString();
+
+                RadioButton2.Checked = false;RadioButton3.Checked = false;RadioButton4.Checked = false;
+                RadioButton5.Checked = false;RadioButton6.Checked = false;RadioButton7.Checked = false;
+                RadioButton8.Checked = false;RadioButton9.Checked = false;RadioButton10.Checked = false;
+                RadioButton11.Checked = false;RadioButton12.Checked = false;
+            
+        }
+
+        if (RadioButton2.Checked == true){
+            resultado = valor / kilogramo;
+            TextBox2.Text = resultado.ToString();
+
+            RadioButton1.Checked = false; RadioButton3.Checked = false; RadioButton4.Checked = false;
+            RadioButton5.Checked = false; RadioButton6.Checked = false; RadioButton7.Checked = false;
+            RadioButton8.Checked = false; RadioButton9.Checked = false; RadioButton10.Checked = false;
+            RadioButton11.Checked = false; RadioButton12.Checked = false;
+        }
+
+        if (RadioButton3.Checked == true){
+            resultado = valor * libra;
+            TextBox2.Text = resultado.ToString();
+
+            RadioButton2.Checked = false; RadioButton1.Checked = false; RadioButton4.Checked = false;
+            RadioButton5.Checked = false; RadioButton6.Checked = false; RadioButton7.Checked = false;
+            RadioButton8.Checked = false; RadioButton9.Checked = false; RadioButton10.Checked = false;
+            RadioButton11.Checked = false; RadioButton12.Checked = false;
+        }
+
+        if (RadioButton4.Checked == true){
+            resultado = (valor * libra) * onza;
+            TextBox2.Text = resultado.ToString();
+
+            RadioButton2.Checked = false; RadioButton3.Checked = false; RadioButton1.Checked = false;
+            RadioButton5.Checked = false; RadioButton6.Checked = false; RadioButton7.Checked = false;
+            RadioButton8.Checked = false; RadioButton9.Checked = false; RadioButton10.Checked = false;
+            RadioButton11.Checked = false; RadioButton12.Checked = false;
+        }
+
+        if (RadioButton5.Checked == true ){
+            resultado = (valor * kilogramo) * onza;
+            TextBox2.Text = resultado.ToString();
+
+            RadioButton2.Checked = false; RadioButton3.Checked = false; RadioButton4.Checked = false;
+            RadioButton1.Checked = false; RadioButton6.Checked = false; RadioButton7.Checked = false;
+            RadioButton8.Checked = false; RadioButton9.Checked = false; RadioButton10.Checked = false;
+            RadioButton11.Checked = false; RadioButton12.Checked = false;
+        }
+
+        if (RadioButton6.Checked == true){
+            resultado = valor / pies;
+            TextBox2.Text = resultado.ToString();
+
+            RadioButton2.Checked = false; RadioButton3.Checked = false; RadioButton4.Checked = false;
+            RadioButton5.Checked = false; RadioButton1.Checked = false; RadioButton7.Checked = false;
+            RadioButton8.Checked = false; RadioButton9.Checked = false; RadioButton10.Checked = false;
+            RadioButton11.Checked = false; RadioButton12.Checked = false;
+        }
+
+        if (RadioButton7.Checked == true){
+            resultado = (valor * pies) * pulgadas;
+            TextBox2.Text = resultado.ToString();
+
+            RadioButton2.Checked = false; RadioButton3.Checked = false; RadioButton4.Checked = false;
+            RadioButton5.Checked = false; RadioButton6.Checked = false; RadioButton1.Checked = false;
+            RadioButton8.Checked = false; RadioButton9.Checked = false; RadioButton10.Checked = false;
+            RadioButton11.Checked = false; RadioButton12.Checked = false;
+        }
+
+        if (RadioButton8.Checked == true){
+            resultado = valor / kilometros;
+            TextBox2.Text = resultado.ToString();
+
+            RadioButton2.Checked = false; RadioButton3.Checked = false; RadioButton4.Checked = false;
+            RadioButton5.Checked = false; RadioButton6.Checked = false; RadioButton7.Checked = false;
+            RadioButton1.Checked = false; RadioButton9.Checked = false; RadioButton10.Checked = false;
+            RadioButton11.Checked = false; RadioButton12.Checked = false;
+        }
+
+        if (RadioButton9.Checked == true){
+            resultado = (valor * kilometros) * (pulgadas * kilometros);
+            TextBox2.Text = resultado.ToString();
+
+            RadioButton2.Checked = false; RadioButton3.Checked = false; RadioButton4.Checked = false;
+            RadioButton5.Checked = false; RadioButton6.Checked = false; RadioButton7.Checked = false;
+            RadioButton8.Checked = false; RadioButton1.Checked = false; RadioButton10.Checked = false;
+            RadioButton11.Checked = false; RadioButton12.Checked = false;
+        }
+
+        if (RadioButton10.Checked == true)
+        {
+            resultado = valor * pesos;
+            TextBox2.Text = resultado.ToString();
+            RadioButton2.Checked = false; RadioButton3.Checked = false; RadioButton4.Checked = false;
+            RadioButton5.Checked = false; RadioButton6.Checked = false; RadioButton7.Checked = false;
+            RadioButton8.Checked = false; RadioButton9.Checked = false; RadioButton1.Checked = false;
+            RadioButton11.Checked = false; RadioButton12.Checked = false;
+        }
+
+        if (RadioButton11.Checked == true)
+        {
+            resultado = valor * 59.15;
+            TextBox2.Text = resultado.ToString();
+
+            RadioButton2.Checked = false; RadioButton3.Checked = false; RadioButton4.Checked = false;
+            RadioButton5.Checked = false; RadioButton6.Checked = false; RadioButton7.Checked = false;
+            RadioButton8.Checked = false; RadioButton9.Checked = false; RadioButton10.Checked = false;
+            RadioButton1.Checked = false; RadioButton12.Checked = false;
+        }
+
+        if (RadioButton12.Checked == true)
+        {
+            resultado = valor * 44.25;
+            TextBox2.Text = resultado.ToString();
+
+            RadioButton2.Checked = false; RadioButton3.Checked = false; RadioButton4.Checked = false;
+            RadioButton5.Checked = false; RadioButton6.Checked = false; RadioButton7.Checked = false;
+            RadioButton8.Checked = false; RadioButton9.Checked = false; RadioButton10.Checked = false;
+            RadioButton11.Checked = false; RadioButton1.Checked = false;
+        }
+
+    }
+}
